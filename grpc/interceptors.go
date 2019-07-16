a2ec9f934eee25bc75e8d78978c9e0159e5d1cfd
Andrew Rodionoff <andviro@gmail.com>
2019-07-17 01:11:30 +0300

    proper cloning of gRPC metadata

--- grpc/interceptors.go
+++ grpc/interceptors.go
@@ -3,6 +3,8 @@ package grpc
 import (
 	"context"
 	"net/http"
+	"net/textproto"
+	"strings"
 
 	"google.golang.org/grpc"
 	grpcMetadata "google.golang.org/grpc/metadata"
@@ -10,10 +12,25 @@ import (
 	"github.com/go-mixins/metadata"
 )
 
+// MetadataKeyPrefix is prepended to metadata keys on the wire
+var MetadataKeyPrefix = "x-grpc-meta-"
+
 func UnaryClientInterceptor() grpc.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req interface{}, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-		md := metadata.From(ctx)
-		ctx = grpcMetadata.NewOutgoingContext(ctx, grpcMetadata.MD(md))
+		md1 := metadata.From(ctx)
+		md2, _ := grpcMetadata.FromOutgoingContext(ctx)
+		dest := make(grpcMetadata.MD)
+		for k, v := range md2 {
+			vv := make([]string, len(v))
+			copy(vv, v)
+			dest[k] = vv
+		}
+		for k, v := range md1 {
+			vv := make([]string, len(v))
+			copy(vv, v)
+			dest[MetadataKeyPrefix+strings.ToLower(k)] = vv
+		}
+		ctx = grpcMetadata.NewOutgoingContext(ctx, dest)
 		return invoker(ctx, method, req, reply, cc, opts...)
 	}
 }
@@ -21,7 +38,13 @@ func UnaryClientInterceptor() grpc.UnaryClientInterceptor {
 func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (_ interface{}, rErr error) {
 		md, _ := grpcMetadata.FromIncomingContext(ctx)
-		ctx = metadata.With(ctx, http.Header(md))
+		dest := make(http.Header)
+		for k, v := range md {
+			if strings.HasPrefix(k, MetadataKeyPrefix) {
+				dest[textproto.CanonicalMIMEHeaderKey(strings.TrimPrefix(k, MetadataKeyPrefix))] = v
+			}
+		}
+		ctx = metadata.With(ctx, dest)
 		return handler(ctx, req)
 	}
 }
